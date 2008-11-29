4f936bc81460cd4b3bb8c4b9fa063ad42df7ce34
berend.van.berkum@gmail.com <berend.van.berkum@gmail.com@90eedc10-db1d-0410-acd1-494184efba37>
2008-11-29 00:50:25 +0000

    setup.py now doest test too. started some work on dependencies.
    git-svn-id: http://pydelicious.googlecode.com/svn/trunk@28 90eedc10-db1d-0410-acd1-494184efba37

--- setup.py
+++ setup.py
@@ -1,27 +1,55 @@
-##
-# License: pydelicious is released under the bsd license.
-# See 'license.txt' for more informations.
+#!/usr/bin/env python
 #
-
-dev = 0
-if dev:
-	raise Exception, \
-		("""
-This does not work as intended right now.
-Dont use it. Will be fixed.""")
-
-from distutils.core import setup
-import os
+# This file is in the public domain.
+import sys
+from distutils.core import setup, Command
 import pydelicious
 
-setup(
-	name='pydelicious',
-	version=pydelicious.__version__,
-	author=pydelicious.__author__,
-	author_email=pydelicious.__author_email__,
-	url=pydelicious.__url__,
-	description=pydelicious.__description__,
-	long_description=pydelicious.__long_description__,
-	py_modules=['pydelicious'],
-)
 
+class Test(Command):
+    description = 'Run pydelicious API tests.'
+    user_options = []
+
+    def initialize_options(self): pass
+
+    def finalize_options(self): pass
+
+    def run(self):
+        from tests import main
+        main.test_api()
+
+
+# TODO: setuptools is said to do dependencies but that needs to be figured out further
+#from setuptools import setup
+#requires = ['feedparser']
+#if sys.version[:2] == (2, 4):
+#    requires += ["elementtree >= 1.2"]
+#
+#elif sys.version[:2] == (2, 6):
+#    pass # integrated into the standard library as xml.etree.
+
+# TODO: need to see this work...
+dependency_links = [
+    "http://feedparser.org/feedparser.py#egg=feedparser-latest"
+]
+
+
+### distutils setup
+
+setup(
+    cmdclass = {
+        'test': Test,
+    },
+    name = 'pydelicious',
+    version = pydelicious.__version__,
+    license = 'BSD',
+    description = pydelicious.__description__,
+    long_description = pydelicious.__long_description__,
+
+    author = pydelicious.__author__,
+    author_email = pydelicious.__author_email__,
+    url = pydelicious.__url__,
+
+    py_modules = [ 'pydelicious' ],
+#    requires = requires
+)
