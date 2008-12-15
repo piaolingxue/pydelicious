acc5501c8dc8bca312f0194b5dcfcae8ab7006c3
berend.van.berkum@gmail.com <berend.van.berkum@gmail.com@90eedc10-db1d-0410-acd1-494184efba37>
2008-12-15 00:09:02 +0000

    This does install dlcs and has an untested caching handler.
    git-svn-id: http://pydelicious.googlecode.com/svn/trunk@37 90eedc10-db1d-0410-acd1-494184efba37

---
+++ setup_tools.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""setup_tools - manages installation for pydelicious-tools, located in the 
+tools/ directory.
+
+(not to be confused with setuptools, which is the distutils extension on which
+the installer is based).
+
+This file is in the public domain.
+"""
+from setuptools import setup
+
+# http://www.bud.ca/blog/how-to-install-python-shell-scripts-with-buildout
+# writes it is better to use buildout, but this works for me (@ Debian
+# GNU/Linux)
+
+# XXX: this has no uninstall, and using a package gets slightly more complicated
+# than a module
+
+setup(
+    name = 'Pydelicious Tools',
+    version = '0.1',
+    license = 'BSD',
+    description = '',
+    long_description = """
+    """,
+
+    author = 'B. van Berkum',
+    author_email = 'berend+pydelicious@dotmpe.com',
+    url = 'http://code.google.com/p/pydelicious/',
+
+    install_requires = ['pydelicious >=0.5.1'],
+
+    packages = ['pydelicious.tools'],
+    package_dir = {'pydelicious.tools': 'tools'},
+
+    entry_points = {
+        'console_scripts': [
+            'dlcs = pydelicious.tools.dlcs:main'
+        ]
+    }
+)
+
